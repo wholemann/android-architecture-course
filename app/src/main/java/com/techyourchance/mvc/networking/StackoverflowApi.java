3b27bfe37b0920aa35fe37c969b81e2723830b33
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-07-24 11:45:39 +0300

    Straightforward implementation of questions list screen

---
+++ app/src/main/java/com/techyourchance/mvc/networking/StackoverflowApi.java
@@ -0,0 +1,13 @@
+package com.techyourchance.mvc.networking;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface StackoverflowApi {
+
+    @GET("/questions?sort=activity&order=desc&site=stackoverflow&filter=withbody")
+    Call<QuestionsListResponseSchema> fetchLastActiveQuestions(@Query("pagesize") Integer pageSize);
+
+}
